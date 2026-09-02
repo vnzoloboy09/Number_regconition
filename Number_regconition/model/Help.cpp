0aee1be9f45632a2597e113f177690d16f98af46
Thanh Dat <vnzoloboy09@gmail.com>
2026-09-02 20:00:44 +0700

    [Dat] add: ReLU for vector and some file read func

--- Number_regconition/model/Help.cpp
+++ Number_regconition/model/Help.cpp
@@ -1,17 +1,104 @@
 #include "Help.h"
 
+#include <fstream>
+#include <iostream>
+
 std::vector<float> Softmax(const std::vector<float>& input) {
-	size_t n = input.size();
-	std::vector<float> output(n);
+    int n = input.size();
+    std::vector<float> output(n);
 
-	float sum = 0.0f;
-	for (auto& i : input) {
-		sum += std::exp(i);
-	}
+    float maxVal = *std::max_element(input.begin(), input.end());
 
-	for (size_t i = 0; i < n; i++) {
-		output[i] = std::exp(input[i]) / sum;
-	}
+    float sum = 0.0f;
+    for (auto& i : input) {
+        sum += std::exp(i - maxVal);
+    }
 
-	return output;
+    for (int i = 0; i < n; i++) {
+        output[i] = std::exp(input[i] - maxVal) / sum;
+    }
+
+    return output;
+}
+
+
+int32_t readInt(std::ifstream& f) {
+    unsigned char bytes[4];
+    f.read((char*)bytes, 4);
+    return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+}
+
+std::vector<std::vector<float>> readMNISTImages(const std::string& filename) {
+    std::ifstream f(filename, std::ios::binary);
+    if (!f.is_open()) {
+        throw std::runtime_error("Could not open file: " + filename);
+    }
+    int magic = readInt(f);
+    int numImages = readInt(f);
+    int rows = readInt(f);
+    int cols = readInt(f);
+
+    std::cout << "Magic=" << magic
+        << " NumImages=" << numImages
+        << " Rows=" << rows
+        << " Cols=" << cols << std::endl;
+
+    std::vector<std::vector<uint8_t>> images(numImages, std::vector<uint8_t>(rows * cols));
+    for (int i = 0; i < numImages; i++) {
+        f.read((char*)images[i].data(), rows * cols);
+    }
+
+    std::vector<std::vector<float>> imgFloat(images.size());
+
+    for (int i = 0; i < images.size(); i++) {
+        imgFloat[i].resize(28 * 28);
+        for (int j = 0; j < images[i].size(); j++) {
+            imgFloat[i][j] = static_cast<float>(images[i][j]) / 255.0f;
+        }
+    }
+
+    return imgFloat;
+}
+
+std::vector<std::vector<float>> readMNISTLabels(const std::string& filename) {
+    std::ifstream f(filename, std::ios::binary);
+    int magic = readInt(f);
+    int numLabels = readInt(f);
+
+    std::vector<uint8_t> labels(numLabels);
+    f.read((char*)labels.data(), numLabels);
+
+    std::vector<std::vector<float>> labelFloat(labels.size());
+
+    for (size_t i = 0; i < labels.size(); i++) {
+        labelFloat[i].resize(10, 0.0f);
+        labelFloat[i][labels[i]] = 1.0f;
+    }
+
+    return labelFloat;
+}
+
+std::vector<uint8_t> readMNISTLabelsOrigin(const std::string& filename) {
+    std::ifstream f(filename, std::ios::binary);
+    int magic = readInt(f);
+    int numLabels = readInt(f);
+
+    std::vector<uint8_t> labels(numLabels);
+    f.read((char*)labels.data(), numLabels);
+
+    return labels;
+}
+
+void drawImage(const std::vector<float>& image, int rows, int cols) {
+    const std::string shades = " .:-=+*#%@";
+    // from light (space) to dark (@)
+
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            float val = image[r * cols + c]; // pixel value
+            int idx = static_cast<int>(val * (shades.size() - 1));
+            std::cout << shades[idx];
+        }
+        std::cout << "\n";
+    }
 }
--- Number_regconition/model/Help.h
+++ Number_regconition/model/Help.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <cmath>
+#include <string>
+#include <cstdint>
 
 inline float ReLU(float x) {
 	return std::max(0.0f, x);
@@ -11,4 +13,23 @@ inline float ReLUDerivertive(float x) {
 	return (x > 0 ? 1.0f : 0.0f);
 }
 
+std::vector<float> ReLU(const std::vector<float>& z) {
+	std::vector<float> out(z.size());
+	for (size_t i = 0; i < z.size(); i++) {
+		out[i] = ReLU(z[i]);
+	}
+
+	return out;
+}
+
 std::vector<float> Softmax(const std::vector<float>& input);
+
+int32_t readInt(std::ifstream& f);
+
+std::vector<std::vector<float>> readMNISTImages(const std::string& filename);
+
+std::vector<std::vector<float>> readMNISTLabels(const std::string& filename);
+
+std::vector<uint8_t> readMNISTLabelsOrigin(const std::string& filename);
+
+void drawImage(const std::vector<float>& image, int rows = 28, int cols = 28);
