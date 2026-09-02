9188f7500d883e6bd5cd597e9618d23adeff7f49
Thanh Dat <vnzoloboy09@gmail.com>
2026-09-02 21:09:38 +0700

    [Dat] add: train, test folder and Model.bin

--- Number_regconition/Main.cpp
+++ Number_regconition/Main.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 
 #include "model/Network.h"
+#include "model/Help.h"
 
 int main()
 {
-    std::vector<std::vector<float>> X = {
-        {1.0f, 1.0f},
-        {1.0f, 0.0f},
-        {0.0f, 1.0f},
-        {0.0f, 0.0f}
-    };
-    
-    std::vector<float> out(2);
-    Network net("model/Model.bin");
-    for (auto& i : X) {
-        net.Forward(i);
-         out = net.GetOutput();
+    srand(42);
 
-        for(auto& x : out) {
-            std::cout << x << ' ';
+    //Network net({ 28 * 28, 16, 16, 10 });
+
+    //auto trainImg = readMNISTImages("train/images.idx3-ubyte");
+    //auto trainLabel = readMNISTLabels("train/labels.idx1-ubyte");
+
+    //net.Train(trainImg, trainLabel, 0.01f, 1);
+    //net.Save("model/Model.bin");
+
+    Network net("model/Model.bin");
+    auto testImg = readMNISTImages("test/images.idx3-ubyte");
+    auto testlabel = readMNISTLabelsOrigin("test/labels.idx1-ubyte");
+
+    std::vector<float> out;
+    int cnt = 0;
+    for (size_t i = 0; i < testImg.size(); i++) {
+        net.Forward(testImg[i]);
+        out = net.GetOutput();
+        float m = 0.0f;
+        uint8_t ans = 0;
+        for (uint8_t x = 0; x < out.size(); x++) {
+            if (out[x] > m) {
+                m = out[x];
+                ans = x;
+            }
         }
-        std::cout << '\n';
+        std::cout << static_cast<int>(ans) << " - " << static_cast<int>(testlabel[i]) << '\n';
+        if (ans == testlabel[i]) {
+            cnt++;
+        }
     }
+    std::cout << cnt << "/" << testImg.size() << '\n';
 
     return 0;
 }
