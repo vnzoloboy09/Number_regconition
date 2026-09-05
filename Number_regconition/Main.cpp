5fe08d2a585ab486b1072bcb42e3e1aa48c8ac46
Thanh Dat <vnzoloboy09@gmail.com>
2026-09-05 22:48:04 +0700

    [Dat] remove: unessesary code

--- Number_regconition/Main.cpp
+++ Number_regconition/Main.cpp
@@ -20,27 +20,6 @@ int main()
     auto testImg = readMNISTImages("test/images.idx3-ubyte");
     auto testlabel = readMNISTLabelsOrigin("test/labels.idx1-ubyte");
 
-    //std::vector<float> out;
-    //int cnt = 0;
-    //for (size_t i = 0; i < testImg.size(); i++) {
-    //    net.Forward(testImg[i]);
-    //    out = net.GetOutput();
-    //    float m = 0.0f;
-    //    uint8_t ans = 0;
-    //    for (uint8_t x = 0; x < out.size(); x++) {
-    //        if (out[x] > m) {
-    //            m = out[x];
-    //            ans = x;
-    //        }
-    //    }
-    //    std::cout << static_cast<int>(ans) << " - " << static_cast<int>(testlabel[i]) << '\n';
-    //    if (ans == testlabel[i]) {
-    //        cnt++;
-    //    }
-    //}
-
-    //std::cout << cnt << "/" << testImg.size() << '\n';
-
     Application app;
     app.Run();
 
